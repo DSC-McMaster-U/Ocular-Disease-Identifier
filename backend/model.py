b6e84c25bfa6c2f608d88d5d3870f01512bfa028
nzajk <nzajkeskovic@outlook.com>
2024-10-12 14:39:56 -0400

    Created a working binary classification model

--- backend/model.py
+++ backend/model.py
@@ -1,19 +1,37 @@
+"""
+- comments regarding model design choices - 
+
+* conv2d: the amount of filters increase as the model goes deeper, this helps with detecting more complex patterns in the data
+* conv2d: chose a kernel size of 3x3 as it is computationally efficient
+* conv2d: relu is used again for computational efficiency
+
+* maxpooling2d: reduces dimensions by half and helps prevent overfitting
+
+* dense: sigmoid function is used for binary classification
+"""
+
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
 input_size = (0, 0, 0) # size of input data 
 
-# build model (work in progress)
+# model for binary classification of ocular disease
 model = models.Sequential([
-    layers.Input(input_shape=input_size), # input layer
-    
-    # add hidden layers here
-    
-    layers.Dense(1, activation='sigmoid') # output layer, sigmoid function for binary classification
+    layers.Input(input_shape=input_size),
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_size),
+    layers.MaxPooling2D((2, 2)), 
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(1, activation='sigmoid')
 ])
 
 # compile model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', 'loss'])
 
-# train model, data is needed for this step
+# train model (data is needed for this step)
 # model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
