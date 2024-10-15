09264e0b66a428145d7d7447fe7210c3295d9e4a
Samuel Shi <samuelshiwork@gmail.com>
2024-10-15 13:39:58 -0400

    Working primitive image upload and displaying on the screen

--- frontend/src/Components/UploadPage.tsx
+++ frontend/src/Components/UploadPage.tsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 const UploadPage = () => {
-  return <div>UploadPage</div>;
+  const [images, setImages] = useState<File[]>([]);
+
+  return (
+    <div>
+      <h1>Upload image</h1>
+      <div>
+        <input
+          type="file"
+          name="imageUpload"
+          id=""
+          onChange={(event) => {
+            setImages(
+              event.target.files
+                ? [...images, ...event.target.files]
+                : [...images]
+            );
+          }}
+        />
+
+        <div>
+          {images &&
+            images.map((image) => <img src={URL.createObjectURL(image)}></img>)}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UploadPage;
