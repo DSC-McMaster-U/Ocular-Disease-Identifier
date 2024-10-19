e4eadfc5106be52f9527ee2fe339f6b7aaa5df96
Jasimraza Momin <jasimrazamomin@Jasimrazas-MacBook-Air.local>
2024-10-19 18:38:02 -0400

    got a POST route working to get images and return a confirmation message, tested using postman

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +15,13 @@ def test_post():
     data = request.get_json()
     input = data['message']
     return jsonify(text=f"{input} - Got this"), 200
+
+@app.route('/image_posting',methods=['POST'])
+def image_posting():
+    image = request.files['image']
+    display_image = Image.open(image)
+    display_image.show()
+    return jsonify(text=f"Image '{image.filename}' received"), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=1000)
