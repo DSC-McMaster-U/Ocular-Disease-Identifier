354dcb3ae8912d23f8cc708536c5ce9ccdca1d0a
IainMac3 <iainhmacdonald@gmail.com>
2024-11-05 16:16:35 -0500

    changed preprocess

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
 
-def preprocess_image(uploaded_image_path):
-    #gotta rewrite this
-    return 
+def preprocess(filename):
+    if (filename.endswith('.jpg') or filename.endswith('.png')):
+        # Load the image using OpenCV (returns the image as a NumPy array)
+        img = cv2.imread(filename)
+
+        # Resize the image
+        img_resized = cv2.resize(img, (100, 100))
+
+        # Normalize
+        img_normalized = img_resized / 255.0
+        return img_normalized
